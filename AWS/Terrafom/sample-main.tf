b68b5e2696cf9e380bd3fb54f2d77c5e97a82ead
Harshal Rembhotkar <harshalrembhotkar94@gmail.com>
2024-09-09 23:41:24 +0530

    Terraform added

---
+++ AWS/Terrafom/sample-main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "ap-south-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "example" {
+    
+    ami = "ami-0c55b159cbfafe1f0"   # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+    subnet_id = "subnet_ID"         # Specify an appropriate ID
+    key_name = "<key_pair>"         
+
+    
+}
